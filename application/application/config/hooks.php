dc570519a03a1b0493341b8e87f6c22438c377f3
Miguel <miguelgonzalezgomez@gmail.com>
2014-06-23 23:13:01 +0200

    Añadiendo conf. para tests con Travis

--- application/application/config/hooks.php
+++ application/application/config/hooks.php
@@ -10,7 +10,13 @@
 |
 */
 
-
+// application/config/hooks.php
+$hook['display_override'] = array(
+	'class' => 'DisplayHook',
+	'function' => 'captureOutput',
+	'filename' => 'DisplayHook.php',
+	'filepath' => 'hooks'
+);
 
 /* End of file hooks.php */
 /* Location: ./application/config/hooks.php */
