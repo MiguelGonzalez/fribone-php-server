6d476091bb42194a135150bcb4e6e1def4900e30
Miguel <miguelgonzalezgomez@gmail.com>
2014-06-24 22:06:14 +0200

    Añadido comprobación si es testing para añadir librerías Mockup de tests

--- application/application/core/MY_Controller.php
+++ application/application/core/MY_Controller.php
@@ -10,6 +10,14 @@ class MY_Controller extends CI_Controller {
 
     function __construct() {
         parent::__construct();
+
+        if (defined('ENVIRONMENT')) {
+            if(ENVIRONMENT == 'testing') {
+                $this->load->add_package_path(APPPATH.'tests');
+            }
+        }
+
+        $this->load->model('User');
     }
 
     protected function _renderJson($json) {
