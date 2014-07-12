334c0e9614785817021f9b962455a8ca2ad10379
Miguel <miguelgonzalezgomez@gmail.com>
2014-07-12 21:43:46 +0200

    Corrección paréntesis para el commit anterior

--- application/application/config/config.php
+++ application/application/config/config.php
@@ -362,7 +362,7 @@ $config['proxy_ips'] = '';
 */
 function __autoload($class) {
     if (substr($class,0,3) !== 'CI_') {
-        if (file_exists($file = APPPATH . 'core/' . $class . EXT) {
+        if (file_exists($file = APPPATH . 'core/' . $class . EXT)) {
             include $file;
         }
     }
