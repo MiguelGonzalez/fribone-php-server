6e042241df6b47c222cd86c9904b4c17a7d7e322
Miguel <miguelgonzalezgomez@gmail.com>
2014-07-18 20:58:07 +0200

    Quitado Ember.js de las vistas y del js

--- application/application/views/tablon/template/skeleton.php
+++ application/application/views/tablon/template/skeleton.php
@@ -9,7 +9,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title><?php echo $title;?></title>
         <meta name="description" content="">
-        
+
         <link href='http://fonts.googleapis.com/css?family=Dancing+Script' rel='stylesheet' type='text/css'>
         <link rel="stylesheet" href="<?php echo base_url('resources/css/normalize.css');?>">
         <link rel="stylesheet" href="<?php echo base_url('resources/css/main.css');?>">
@@ -21,62 +21,39 @@
             <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
 
-        <script type="text/x-handlebars">
-            <div class="container-fluid">
-                <header class="navbar navbar-static-top" id="top" role="banner">
-                    <div class="container">
-                        <?php echo $header;?>
-                    </div>
-                </header>
-
-                <div id="content-center">
-                    <div class="container">
-                        <div class="col-md-2" role="main">
-                            <div id="menu-left">
-                                <div class="list-group">
-                                    {{#link-to 'fridge' class="list-group-item"}}Mi frigorífico{{/link-to}}
-                                    {{#link-to 'statistics' class="list-group-item"}}Estadísticas{{/link-to}}
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-10" role="main">
-                            <div id="main" class="clearfix col-md-12">
-                                {{outlet}}
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="push-footer"></div>
-            </div>
-
-            <footer class="footer" role="contentinfo">
+        <div class="container-fluid">
+            <header class="navbar navbar-static-top" id="top" role="banner">
                 <div class="container">
-                    <div class="wrapper">
-                        <?php echo $footer;?>
-                    </div>
+                    <?php echo $header;?>
                 </div>
-            </footer>
-            {{outlet modal}}
+            </header>
 
-            <div class="modal fade" tabindex="-1" role="dialog">
-                <div class="modal-dialog modal-sm">
-                    <div class="modal-content">
-                        <div class="modal-header">
-
+            <div id="content-center">
+                <div class="container">
+                    <div class="col-md-2" role="main">
+                        <div id="menu-left">
+                            <div class="list-group">
+                                <?php echo $body;?>
+                            </div>
                         </div>
-                        <div class="modal-body">
+                    </div>
+                    <div class="col-md-10" role="main">
+                        <div id="main" class="clearfix col-md-12">
 
-                        </div>
-                        <div class="modal-footer">
-                            
                         </div>
                     </div>
                 </div>
             </div>
-            {{bs-growl-notifications}}
-        </script>
+            <div class="push-footer"></div>
+        </div>
 
-        <?php echo $body;?>
+        <footer class="footer" role="contentinfo">
+            <div class="container">
+                <div class="wrapper">
+                    <?php echo $footer;?>
+                </div>
+            </div>
+        </footer>
 
         <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="<?php echo base_url('resources/js/jquery-2.1.1.min.js');?>"><\/script>')</script>
