5eabe3919be06c3161d2778553f7995f2c8e22a6
Miguel <miguelgonzalezgomez@gmail.com>
2014-07-20 12:10:11 +0200

    Cambiado truncate de tablas

--- application/application/tests/database_inflater_truncate.sql
+++ application/application/tests/database_inflater_truncate.sql
@@ -3,6 +3,7 @@ SET FOREIGN_KEY_CHECKS = 0;
 TRUNCATE my_user;
 TRUNCATE my_user_remember_token;
 TRUNCATE my_user_frigorifico;
+TRUNCATE my_user_frigorifico_producto;
 TRUNCATE my_compra;
 TRUNCATE my_compra_producto;
 TRUNCATE my_compra_retirada;
