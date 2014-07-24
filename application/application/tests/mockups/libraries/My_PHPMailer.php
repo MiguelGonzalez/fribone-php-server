97ce111b365f6a5e2dea580223e51257806d048d
Miguel <miguelgonzalezgomez@gmail.com>
2014-07-24 22:21:47 +0200

    Mockup para phpmailer

---
+++ application/application/tests/mockups/libraries/My_PHPMailer.php
@@ -0,0 +1,7 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class My_PHPMailer {
+    public function send_email($email, $subject, $data, $name_template, $email_name = NULL) {
+        return TRUE;
+    }
+}
